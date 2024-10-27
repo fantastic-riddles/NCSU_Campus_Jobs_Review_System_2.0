d0cd180444c0fea635ab652611744e21d65d5f88
vihar2712 <viharshah222@gmail.com>
2024-10-27 16:53:32 -0400

    added user and application to model

--- app/models.py
+++ app/models.py
@@ -12,3 +12,20 @@ class Reviews(db.Model):
     review = db.Column(db.String(120), index=True, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     recommendation = db.Column(db.Integer, nullable=False)
+    
+class User(db.Model):
+    """Model which stores the information of the reviews submitted"""
+    __tablename__ = 'users'
+    user_name = db.Column(db.String(10), nullable=False, primary_key=True)
+    name = db.Column(db.String(64),  index=True, nullable=False)
+    email = db.Column(db.String(64), index=True, nullable=False)
+    password = db.Column(db.String(24), index=True, nullable=False)
+    type = db.Column(db.String(10), index=True, nullable=False)
+    
+
+    
+class Application(db.Model):
+    """Model which stores the information of the reviews submitted"""
+    __tablename__ = 'applications'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'))
