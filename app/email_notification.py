731eb79365385f9b852680a6f58856874532b2d0
vihar2712 <viharshah222@gmail.com>
2024-11-01 15:14:53 -0400

    changes in contact page and email.py file

--- app/email_notification.py
+++ app/email_notification.py
@@ -69,7 +69,7 @@ def send_welcome_email(email, username, password, is_employee=False):
         mssg = mssg.replace("<username>", username).replace("<password>", password)
         
         message = Mail(
-        from_email=os.environ.get('EMAIL_ADDRESS'),
+        from_email="viharshah222@gmail.com",
         to_emails=email,
         subject='Welcome to NCSU Job Portal account!',
         html_content=f"{mssg}")
